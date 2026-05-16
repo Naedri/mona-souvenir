295c61950d5eb3c05905059bc7531a5056612d15
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-05-16 18:39:00 +0200

    refactor(type): interface museum allow search

--- src/server/services/museums/providers/louvre.provider.ts
+++ src/server/services/museums/providers/louvre.provider.ts
@@ -2,24 +2,20 @@ import type { ArtworkDto } from "../dto/artwork.dto";
 import type { SearchArtworksDto } from "../dto/search-artworks.dto";
 import type { MuseumProvider } from "./museum-provider.interface";
 
-import { searchLouvreApi } from "../clients/louvre.client";
-import { mapLouvreArtwork } from "../mappers/louvre.mapper";
-
 import { debug } from "@/server/utils/debug";
 
 export class LouvreProvider implements MuseumProvider {
   readonly museumId = "louvre";
 
+  displayName = "Musée du Louvre";
+
+  supportsSearch = false;
+
+  supportsArtworkDetails = true;
+
   async searchArtworks(input: SearchArtworksDto): Promise<ArtworkDto[]> {
-    debug.log("louvre.provider", "search artworks", input);
-    const response = await searchLouvreApi(input.query);
-    debug.log("louvre.provider", "api response", response);
-    if (!response.hits) {
-      debug.warn("louvre.provider", "missing hits array", response);
-      return [];
-    }
-    const artworks = response.hits.map(mapLouvreArtwork);
-    debug.log("louvre.provider", "mapped artworks", artworks);
-    return artworks;
+    debug.warn("louvre.provider", "search unsupported", input);
+
+    return [];
   }
 }
--- src/server/services/museums/providers/museum-provider.interface.ts
+++ src/server/services/museums/providers/museum-provider.interface.ts
@@ -4,5 +4,11 @@ import type { SearchArtworksDto } from "../dto/search-artworks.dto";
 export interface MuseumProvider {
   readonly museumId: string;
 
+  displayName: string;
+
+  supportsSearch: boolean;
+
+  supportsArtworkDetails: boolean;
+
   searchArtworks(input: SearchArtworksDto): Promise<ArtworkDto[]>;
 }
