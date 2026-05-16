f54dad3755de23321c846c577b4c4889ee4b6ba7
JALLAIS Adrien <adrien.jallais@protonmail.com>
2026-05-16 10:22:00 +0200

    feat(auth): better auth route session

---
+++ src/server/auth/index.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { env } from "@/env";
+import { db } from "@/server/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg"
+  }),
+
+  emailAndPassword: {
+    enabled: true
+  },
+
+  secret: env.BETTER_AUTH_SECRET,
+
+  baseURL: env.BETTER_AUTH_URL
+});
